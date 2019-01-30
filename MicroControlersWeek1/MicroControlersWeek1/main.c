d93354ddc169c7a626f78f53cf78feea65d4d6a5
Unknown <rwoldhui@avans.nl>
2019-01-30 15:04:47 +0100

    [Added] Opdracht up to and including B.7b

--- MicroControlersWeek1/MicroControlersWeek1/main.c
+++ MicroControlersWeek1/MicroControlersWeek1/main.c
@@ -9,6 +9,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
 
 void wait(int ms);
 /* OPDRACHT: B.2
@@ -71,38 +72,108 @@ wait(100);
 return 1;
 }*/
 
-/* OPDRACHT B.5*/
+/* OPDRACHT B.5
 typedef struct {
-	unsigned char data;
-	unsigned int delay ;
+unsigned char data;
+unsigned int delay ;
 } PATTERN_STRUCT;
 
 PATTERN_STRUCT pattern[] = {
-	{0x00, 100}, {0x01, 100}, {0x02, 100}, {0x04, 100}, {0x10, 100}, {0x20, 100}, {0x40, 100}, {0x80, 100},
-	{0x00, 100},
-	{0xAA,  50}, {0x55,  50},
-	{0xAA,  50}, {0x55,  50},
-	{0xAA,  50}, {0x55,  50},
-	{0x00, 100},
-	{0x81, 100}, {0x42, 100}, {0x24, 100}, {0x18, 100}, {0x0F, 200}, {0xF0, 200}, {0x0F, 200}, {0xF0, 200},
-	{0x00, 0x00}
+{0x00, 100}, {0x01, 100}, {0x02, 100}, {0x04, 100}, {0x10, 100}, {0x20, 100}, {0x40, 100}, {0x80, 100},
+{0x00, 100},
+{0xAA,  50}, {0x55,  50},
+{0xAA,  50}, {0x55,  50},
+{0xAA,  50}, {0x55,  50},
+{0x00, 100},
+{0x81, 100}, {0x42, 100}, {0x24, 100}, {0x18, 100}, {0x0F, 200}, {0xF0, 200}, {0x0F, 200}, {0xF0, 200},
+{0x00, 0x00}
 };
 
 
 int main(void){
-	DDRD = 0b11111111;
-	
-	while (1==1)
-	{
-		int index = 0;
-		while( pattern[index].delay != 0 ) {
-			PORTD = pattern[index].data;
-			wait(pattern[index].delay);
-			index++;
-		}
-	}
+DDRD = 0b11111111;
 
-	return 1;
+while (1)
+{
+int index = 0;
+while( pattern[index].delay != 0 ) {
+PORTD = pattern[index].data;
+wait(pattern[index].delay);
+index++;
+}
+}
+
+return 1;
+}*/
+
+/*OPDRACHT B.6
+int main(void){
+DDRD = 0b11111111;
+int delay = 1000;
+int fast = 0;
+while (1)
+{
+if(PINC & 0b00000001){
+if(fast == 0){
+delay /= 4;
+fast = 1;
+}else{
+fast = 0;
+delay *=4;
+}
+}
+PORTD = 0b11111111;
+wait(delay);
+PORTD = 0b00000000;
+wait(delay);
+}
+
+return 1;
+}*/
+
+
+/* OPDRACHT B.7b*/
+void setCharliePlexingLed(int lednr)
+{
+	switch (lednr)
+	{
+		case 0:
+		DDRA = 0b00000011;
+		PORTA = 0b00000001;
+		break;
+		case 1:
+		DDRA = 0b00000011;
+		PORTA = 0b00000010;
+		break;
+		case 2:
+		DDRA = 0b00000110;
+		PORTA = 0b00000010;
+		break;
+		case 3:
+		DDRA = 0b00000110;
+		PORTA = 0b00000100;
+		break;
+		case 4:
+		DDRA = 0b00000101;
+		PORTA = 0b00000100;
+		break;
+		case 5:
+		DDRA = 0b00000101;
+		PORTA = 0b00000001;
+		break;
+	}
+}
+
+int main(void){
+	int ledNr = 0;
+	while(1){
+		setCharliePlexingLed(ledNr);
+		ledNr++;
+		if(ledNr > 5){
+			ledNr = 0;
+		}
+		wait(1000);
+	}
 }
 
 
