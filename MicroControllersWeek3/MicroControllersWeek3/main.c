c89a5c024fef00193f0d967e2389dbb042c8ed24
Unknown <rwoldhui@avans.nl>
2019-02-13 10:36:53 +0100

    [Added] week3 project

---
+++ MicroControllersWeek3/MicroControllersWeek3/main.c
@@ -0,0 +1,18 @@
+/*
+ * MicroControllersWeek3.c
+ *
+ * Created: 2/13/2019 10:36:07 AM
+ * Author : 
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
