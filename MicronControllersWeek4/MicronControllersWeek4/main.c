986df04c89e261ca2a288c9f9004c42d5137192c
Unknown <rwoldhui@avans.nl>
2019-02-20 10:24:46 +0100

    [Added] Week 4 base project

---
+++ MicronControllersWeek4/MicronControllersWeek4/main.c
@@ -0,0 +1,18 @@
+/*
+* MicronControllersWeek4.c
+*
+* Created: 2/20/2019 10:24:23 AM
+* Author : Ruben
+*/
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	/* Replace with your application code */
+	while (1)
+	{
+	}
+}
+
