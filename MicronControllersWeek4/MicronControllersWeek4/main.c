46f9fb4d8ea12aae28854e88ab5ec0a1b81cbb3e
Unknown <rwoldhui@avans.nl>
2019-02-20 11:20:33 +0100

    [Added] B1

--- MicronControllersWeek4/MicronControllersWeek4/main.c
+++ MicronControllersWeek4/MicronControllersWeek4/main.c
@@ -5,14 +5,40 @@
 * Author : Ruben
 */
 
+#define F_CPU 8000000
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+void wait( int ms )
+{
+	for (int tms=0; tms<ms; tms++)
+	{
+		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
+	}
+}
+
+// Initialize ADC: 10-bits (left justified), free running
+void adcInit( void )
+{
+	ADMUX = 0b01100001;			// AREF=VCC, result left adjusted, channel1 at pin PF1
+	ADCSRA = 0b11100110;		// ADC-enable, no interrupt, start, free running, division by 64
+}
 
 int main(void)
 {
-	/* Replace with your application code */
-	while (1)
-	{
+	DDRF = 0x00;
+	DDRA = 0xFF;
+	DDRB = 0xFF;
+	
+	adcInit();
+	
+	while(1){
+		PORTB = ADCL;
+		PORTA = ADCH;
+		wait(100);
 	}
+	
 }
+
 
